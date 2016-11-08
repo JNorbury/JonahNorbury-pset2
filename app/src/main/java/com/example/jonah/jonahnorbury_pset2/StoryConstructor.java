d1ca3bfecf995490b7b79cec66f2e55bccf443d0
JNorbury <jonah.norbury@student.uva.nl>
2016-11-08 21:17:12 +0100

    fixed startup crash, load filestream for Story read method

--- app/src/main/java/com/example/jonah/jonahnorbury_pset2/StoryConstructor.java
+++ app/src/main/java/com/example/jonah/jonahnorbury_pset2/StoryConstructor.java
@@ -1,19 +1,35 @@
 package com.example.jonah.jonahnorbury_pset2;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by jonah on 08-Nov-16.
  */
 
-public class StoryConstructor extends Activity {
-
+public class StoryConstructor extends AppCompatActivity {
+    private Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.story_layout);
+        try {
+            resources = getResources();
+            InputStream iS = resources.openRawResource(R.raw.madlib0_simple);
+            Story new_story = new Story(iS);
+        }
+        catch (Resources.NotFoundException e) {
+            Toast toast = Toast.makeText(this, "File: not found!", Toast.LENGTH_LONG);
+            toast.show();
+        }
 
-        setContentView(android.R.layout.);
     }
+
 }
